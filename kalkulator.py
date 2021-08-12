c1b8a8bcf88bb5e9464327cd97969dcf1dcfd056
Grzegorz Wasilko <wasilko1@o2.pl>
2021-08-12 18:10:58 +0200

    TODO logging +

---
+++ kalkulator.py
@@ -0,0 +1,41 @@
+#Moduł 4-zadanie 2-->> Kalkulator
+#string zamiast int rzutowanie i co ?
+
+import sys #do argv w przyszłości
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="logfile-kalkulator.log")
+#bsicConfic ustawiłem aby zwracał komunikaty od poziomu debug +data i godzina + plik txt
+#----------------------------------------------------------------------
+def calculator (arg1,arg2) :
+choice=input("wybierz działanie 1-dodawanie, 2- odejmowanie, 3- Mnożenie, 4- Dzielenie \n")
+if choice == 1: #Dodawanie
+result=arg1+arg2
+return result
+
+elif choice == 2: #Odejmowaie
+if arg2>arg1:
+#logging.info('wynik ponizej zera')
+exit(1) #upewnic sie czy to dobry wybór na błąd !
+elif arg2<=arg1:
+result=arg1-arg2
+return result
+
+elif choice == 3: #Mnożenie
+result=arg1*arg2
+return result
+
+elif choice == 4: #Dzielenie
+if arg2==0:
+logging.info("pamiętaj cholero, nie dziel przez 0")
+elif arg2!=0:
+result=arg1/arg2
+return result
+
+#__________________________________________________________________
+if name == '__main__' :
+
+value_one=int(input('podaj pierwszą liczbę'))
+value_two=int(input('podaj drugą liczbę'))
+print('wprowadziłeś %d %d' % (value_one,value_two))
+response=calculator(value_one,value_two)
+print("wynik działania to %d" % response)
