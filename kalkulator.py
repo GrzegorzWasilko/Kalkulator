d0c3be9c34bed14a1d60ab568d9ddca7ed090b30
Grzegorz Wasilko <wasilko1@o2.pl>
2021-08-13 12:40:55 +0200

    comments add

--- kalkulator.py
+++ kalkulator.py
@@ -8,26 +8,27 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filen
 #----------------------------------------------------------------------
 def calculator (arg1,arg2) :
     choice=input("wybierz działanie 1-dodawanie, 2- odejmowanie, 3- Mnożenie, 4- Dzielenie \n")
-    if choice == 1: #Dodawanie
+    if choice =='1': #Dodawanie
         result=arg1+arg2
+        print(type(result))#typ result to int
         return result
 
-    elif choice == 2: #Odejmowaie
+    elif choice =='2': #Odejmowaie
         if arg2>arg1:
-        #logging.info('wynik ponizej zera')
-         exit(1) #upewnic sie czy to dobry wybór na błąd !
-    elif arg2<=arg1:
-        result=arg1-arg2
-        return result
+         print('wynik ponizej zera')    #logging.info nie działa :((
+         #exit(1) #upewnic sie czy to dobry wybór na błąd !
+        elif arg2<=arg1:
+            result=arg1-arg2
+            return result
 
-    elif choice == 3: #Mnożenie
+    elif choice =='3': #Mnożenie
         result=arg1*arg2
         return result
 
-    elif choice == 4: #Dzielenie
-        if arg2==0:     # warunek nie działa , dzieli przez zero
-          logging.info("pamiętaj cholero, nie dziel przez 0")
-          exit(1)
+    elif choice =='4': #Dzielenie
+        if arg2==0:     # warunek nie działa , dzieli przez zero ?
+          logging.info("pamiętaj cholero, nie dziel przez 0")#komunikat sie nie pojawia
+          exit(1)#wychodzi z programu bez błedu
         elif arg2!=0:
             result=arg1/arg2
             return result
@@ -37,5 +38,5 @@ if __name__== '__main__' :
     value_one=int(input('podaj pierwszą liczbę'))
     value_two=int(input('podaj drugą liczbę'))
     print('wprowadziłeś %d %d' % (value_one,value_two))
-    response=calculator(value_one,value_two)
-    print("wynik działania to %d" % response)
+    response=calculator(int(value_one),int(value_two))#działa i bez rzutowania 
+    print("wynik działania to %s" % response)
