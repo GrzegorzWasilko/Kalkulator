8fd6cf10fb2b5f573ea43e9120c462343b6c2d8d
Grzegorz Wasilko <wasilko1@o2.pl>
2021-08-19 12:37:11 +0200

    new comments

--- kalkulator.py
+++ kalkulator.py
@@ -4,7 +4,7 @@
 import sys #do argv w przyszłości
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="logfile-kalkulator.log")
-#bsicConfic ustawiłem aby zwracał komunikaty od poziomu debug +data i godzina + plik txt
+#bsicConfic ustawiłem aby zwracał komunikaty od poziomu debug +data i godzina + plik txt (zapis do pliku nie na ekran)
 #----------------------------------------------------------------------
 def calculator (arg1,arg2) :
     choice=input("wybierz działanie 1-dodawanie, 2- odejmowanie, 3- Mnożenie, 4- Dzielenie \n")
@@ -26,9 +26,9 @@ def calculator (arg1,arg2) :
         return result
 
     elif choice =='4': #Dzielenie
-        if arg2==0:     # warunek nie działa , dzieli przez zero ?
-          logging.info("pamiętaj cholero, nie dziel przez 0")#komunikat sie nie pojawia
-          exit(1)#wychodzi z programu bez błedu
+        if arg2==0:   
+          logging.info("pamiętaj cholero, nie dziel przez 0")#komunikat pojawi sie w notatniku
+          exit(1)   #rasie Value Error  wychodzi z programu bez błedu
         elif arg2!=0:
             result=arg1/arg2
             return result
